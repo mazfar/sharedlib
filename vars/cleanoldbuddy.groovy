23f86d711cfd0664350dec8607577b2cdf198a53
mazfar2011@gmail.com <mazfar2011@gmail.com>
2020-10-18 18:41:42 +0000

    testing with shell

--- vars/cleanoldbuddy.groovy
+++ vars/cleanoldbuddy.groovy
@@ -1,5 +1,5 @@
 def call(String name = 'human') {
-   command = "git clone " + ${name}
+   command = "git clone " + name
 
 def execute_state=sh(returnStdout: true, script: command)
     //echo "Hello, ${name}."
